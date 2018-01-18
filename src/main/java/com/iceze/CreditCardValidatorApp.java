3f1c0965775be65327da6fdaf91d0c50d279a7ff
Miroslav <miroslav25a@yahoo.co.uk>
2018-01-18 17:38:00 +0000

    update java doc

--- src/main/java/com/iceze/CreditCardValidatorApp.java
+++ src/main/java/com/iceze/CreditCardValidatorApp.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
- *
+ * Main Class
+ * 
+ *@author Miroslav
  */
 @SpringBootApplication(scanBasePackages={"com.iceze"})
 public class CreditCardValidatorApp 
